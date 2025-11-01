5d40eb0f106862a57c14db9b5818b97a20805dc7
akrentz6 <alexkrentz2@gmail.com>
2025-11-01 13:42:35 +0000

    add and mul DTFunction classes

--- torchdt/ops/__init__.py
+++ torchdt/ops/__init__.py
@@ -1,5 +1,8 @@
 from .base import OpsBase, register_op
 
+# import all operation modules to register their implementations
+from . import arithmetic_ops
+
 __all__ = [
     "OpsBase",
     "register_op",
---
+++ torchdt/ops/arithmetic_ops.py
@@ -0,0 +1,39 @@
+import torch
+from torchdt.autograd import DTFunction
+
+class DTAddFunction(DTFunction):
+
+    @staticmethod
+    def forward(ops, x, y):
+        return ops.add(x, y)
+
+    @staticmethod
+    def setup_context(ctx, ops, inputs, output):
+        pass
+
+    @staticmethod
+    def backward(ctx, ops, grad_output):
+        grad_x = grad_output
+        grad_y = grad_output
+
+        return grad_x, grad_y
+
+class DTMulFunction(DTFunction):
+
+    @staticmethod
+    def forward(ops, x, y):
+        return ops.mul(x, y)
+
+    @staticmethod
+    def setup_context(ctx, ops, inputs, output):
+        x, y = inputs
+        ctx.save_for_backward(x, y)
+
+    @staticmethod
+    def backward(ctx, ops, grad_output):
+        x, y = ctx.saved_tensors
+
+        grad_x = ops.mul(grad_output, y)
+        grad_y = ops.mul(grad_output, x)
+
+        return grad_x, grad_y
