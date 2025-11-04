34f0088d7a77baef92b62df1f3f40d88370b5f95
akrentz6 <alexkrentz2@gmail.com>
2025-11-04 18:00:44 +0000

    add sub and div ops and funcs

--- torchdt/funcs/arithmetic_funcs.py
+++ torchdt/funcs/arithmetic_funcs.py
@@ -2,13 +2,43 @@ import torch
 from torchdt import DType
 from torchdt.ops.arithmetic_ops import (
     DTAddFunction,
+    DTSubFunction,
     DTMulFunction,
+    DTDivFunction,
 )
 
 @DType.register_func(torch.add, torch.Tensor.add)
-def dt_add(x, y):
-    return DTAddFunction.apply(x, y)
+def dt_add(input, other, *, alpha=1, out=None):
+    if alpha != 1:
+        other = torch.mul(other, alpha)
+    result = DTAddFunction.apply(input, other)
+
+    if out is not None:
+        return out.copy_(result)
+    return result
+
+@DType.register_func(torch.sub, torch.Tensor.sub)
+def dt_sub(input, other, *, alpha=1, out=None):
+    if alpha != 1:
+        other = torch.mul(other, alpha)
+    result = DTSubFunction.apply(input, other)
+
+    if out is not None:
+        return out.copy_(result)
+    return result
 
 @DType.register_func(torch.mul, torch.Tensor.mul)
-def dt_mul(x, y):
-    return DTMulFunction.apply(x, y)
+def dt_mul(input, other, *, out=None):
+    result = DTMulFunction.apply(input, other)
+
+    if out is not None:
+        return out.copy_(result)
+    return result
+
+@DType.register_func(torch.div, torch.Tensor.div)
+def dt_div(input, other, *, out=None):
+    result = DTDivFunction.apply(input, other)
+
+    if out is not None:
+        return out.copy_(result)
+    return result
--- torchdt/ops/arithmetic_ops.py
+++ torchdt/ops/arithmetic_ops.py
@@ -18,6 +18,23 @@ class DTAddFunction(DTFunction):
 
         return grad_x, grad_y
 
+class DTSubFunction(DTFunction):
+
+    @staticmethod
+    def forward(ops, x, y):
+        return ops.sub(x, y)
+
+    @staticmethod
+    def setup_context(ctx, ops, inputs, output):
+        pass
+
+    @staticmethod
+    def backward(ctx, ops, grad_output):
+        grad_x = grad_output
+        grad_y = ops.neg(grad_output)
+
+        return grad_x, grad_y
+
 class DTMulFunction(DTFunction):
 
     @staticmethod
@@ -35,5 +52,25 @@ class DTMulFunction(DTFunction):
 
         grad_x = ops.mul(grad_output, y)
         grad_y = ops.mul(grad_output, x)
+
+        return grad_x, grad_y
+
+class DTDivFunction(DTFunction):
+
+    @staticmethod
+    def forward(ops, x, y):
+        return ops.div(x, y)
+
+    @staticmethod
+    def setup_context(ctx, ops, inputs, output):
+        x, y = inputs
+        ctx.save_for_backward(x, y)
+
+    @staticmethod
+    def backward(ctx, ops, grad_output):
+        x, y = ctx.saved_tensors
+
+        grad_x = ops.div(grad_output, y)
+        grad_y = ops.neg(ops.div(ops.mul(grad_output, x), ops.mul(y, y)))
 
         return grad_x, grad_y
