ae11ea55f8536d09c92c486a53bbeb8bfb2ce368
akrentz6 <alexkrentz2@gmail.com>
2025-11-11 11:44:22 +0000

    add ops module imports to __init__

--- torchdt/ops/__init__.py
+++ torchdt/ops/__init__.py
@@ -2,6 +2,9 @@ from .base import OpsBase, register_op, register_base_op
 
 # import all operation modules to register their implementations
 from . import arithmetic_ops
+from . import comparison_ops
+from . import unary_ops
+from . import activation_ops
 
 __all__ = [
     "OpsBase",
