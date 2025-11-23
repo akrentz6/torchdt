4de9bc9e802c28efcb7b0e020cb068e5a7514311
akrentz6 <alexkrentz2@gmail.com>
2025-11-23 12:21:39 +0000

    add cpu kernels for binary ops

--- torchdt/csrc/extension.cpp
+++ torchdt/csrc/extension.cpp
@@ -1,6 +1,9 @@
 #include <torch/extension.h>
 #include <torchdt/registry.h>
 
-// for now, no functions are exposed
+#include "binary_ops.h"
+
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.def("add", &add, "Addition for custom dtypes");
+    m.def("mul", &mul, "Multiplication for custom dtypes");
 }
